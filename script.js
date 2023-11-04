d1461d8ba22ee47427912a7b55ba713ad26d2fab
Ritika Mishra <117935386+sidmishra-1609@users.noreply.github.com>
2023-11-04 21:42:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -26,19 +26,19 @@ class Cat extends Animal {
 console.log("purr");
 	}
 }
-// const animal= new Animal("Unknown");
-// console.log(animal.species)
-// animal.makeSound();
+const animal= new Animal("Unknown");
+console.log(animal.species)
+animal.makeSound();
 
-// const dog= new Dog("Dog");
-// console.log(dog.species);
-// dog.makeSound();
-// dog.bark();
+const dog= new Dog("Dog");
+console.log(dog.species);
+dog.makeSound();
+dog.bark();
 
-// const cat =new Cat("Cat");
-// console.log(cat.species);
-// cat.makeSound();
-// cat.purr();
+const cat =new Cat("Cat");
+console.log(cat.species);
+cat.makeSound();
+cat.purr();
 
 // Do not change the code below this line
 window.Animal = Animal;
