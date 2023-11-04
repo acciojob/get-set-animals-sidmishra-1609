1c6345543217788d33fd000d09f2bdc7c30b8a34
Ritika Mishra <117935386+sidmishra-1609@users.noreply.github.com>
2023-11-04 15:11:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species){
+		this.species=species;
+	}
+	makeSound(){
+console.log(this.species);
+	}
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	bark(){
+console.log('woof')}
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	purr(){
+console.log('purr')}
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
