82756a6281adaa33e5d71892992050566e61cf85
Ritika Mishra <117935386+sidmishra-1609@users.noreply.github.com>
2023-11-27 20:05:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,48 @@
 //complete this code
 class Animal {
-	constructor(species){
-		this._species=species;
-	}
-	get species(){
-		return this._species;
-	}
+  constructor(species) {
+    this._species = species;
+  }
 
-	makeSound(){
-		console.log("The " + this.species + " make a sound");
-	}
-}
+  get species() {
+    return this._species;
+  }
 
-class Dog extends Animal {
-  bark(){
-	  
-	  console.log("woof");
+  makeSound() {
+    console.log("The " + this.species + " makes a sound");
   }
 }
 
 class Cat extends Animal {
-	purr(){
-console.log("purr");
-	}
+  purr() {
+    console.log("purr");
+  }
 }
-// const animal= new Animal("Unknown");
-// console.log(animal.species)
-// animal.makeSound();
 
-const dog= new Dog("Dog");
-console.log(dog.species);
-dog.makeSound();
-dog.bark();
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
 
-const cat =new Cat("Cat");
-console.log(cat.species);
-cat.makeSound();
+const animal = new Animal("Unknown");
+console.log(animal.species); 
+animal.makeSound(); 
+
+const cat = new Cat("Cat");
+console.log(cat.species); 
+cat.makeSound(); 
 cat.purr();
+
+const dog = new Dog("Dog");
+console.log(dog.species); 
+dog.makeSound(); 
+dog.bark(); 
+
+// Do not change the code below this line
+window.Animal = Animal;
+window.Dog = Dog;
+window.Cat = Cat;
 
 // Do not change the code below this line
 window.Animal = Animal;
